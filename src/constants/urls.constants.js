25615899e5a78c788e6d9d80c5ef90bfedadabda
Raheem Azeezat <raheem.azeezat@venturegardengroup.com>
2020-05-30 19:10:53 +0100

    updated urls

--- src/constants/urls.constants.js
+++ src/constants/urls.constants.js
@@ -1,5 +1,27 @@
-export const LOGIN="Account/login"
-export const FORGOT_PASSWORD="ForgotPassword"
-export const GET_ALL_COMPANIES="Company/GetAll"
-export const GET_COMPANY_BY_ID="GetCompanyById"
-
+//Account
+export const LOGIN = "Account/login";
+export const FORGOT_PASSWORD = "Account/ForgotPassword";
+export const RESET_PASSWORD = "Account/ResetPassword";
+export const CHANGE_PASSWORD = "Account/ChangePassword";
+export const VERIFY_ACCOUNT = "Account/verifyaccount";
+//User
+export const CREATE_USER = "User/Create";
+export const UPDATE_USER = "User/Update";
+export const USERS = "User/GetAll";
+export const GET_USER_BY_ID = "GetUserById";
+export const ACTIVATE_USER = "User/ToggleActivateUser";
+//Companies
+export const CREATE_COMPANY = "Company/Create";
+export const UPDATE_COMPANY = "Company/Update";
+export const GET_ALL_COMPANIES = "Company/GetAll";
+export const GET_COMPANY_BY_ID = "GetCompanyById";
+//Department
+export const CREATE_DEPARTMENT = "Department/Create";
+export const UPDATE_DEPARTMENT = "Department/Update";
+export const GET_ALL_DEPARTMENTS = "Department/GetAll";
+export const GET_DEPARTMENT_BY_ID = "GetDepartmentById";
+//Job Role
+export const CREATE_JOBROLE = "JobRole/Create";
+export const UPDATE_JOBROLE = "JobRole/Update";
+export const GET_ALL_JOBROLES = "JobRole/GetAll";
+export const GET_JOBROLE_BY_ID = "GetJobRoleById";
